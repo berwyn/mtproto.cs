c87cebe045f2be968ae46d1eace181d5b4716ef9
berwyn <berwyn.codeweaver@gmail.com>
2015-11-09 19:02:20 -0700

    Adds TLTrue

---
+++ MTProto Tests/TL/TLTrueTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MTProto.TL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTProto_Tests.TL
+{
+    [TestClass]
+    public class TLTrueTests
+    {
+
+        [TestMethod]
+        public void TLTrueSerialization()
+        {
+            var tltrue = new TLTrue();
+            var expectedBuffer = BitConverter.GetBytes(0x3fedd339);
+            CollectionAssert.AreEquivalent(expectedBuffer, tltrue.ToBytes());
+
+            using (var stream = new MemoryStream())
+            {
+                tltrue.ToStream(stream);
+                stream.Position = 0;
+
+                var actualBuffer = new byte[4];
+                stream.Read(actualBuffer, 0, 4);
+                CollectionAssert.AreEquivalent(expectedBuffer, actualBuffer);
+            }
+        }
+
+        [TestMethod]
+        public void TLTrueHydration()
+        {
+            var pos = 0;
+            var buffer = BitConverter.GetBytes(0x3fedd339);
+            var tltrue = new TLTrue(buffer, ref pos);
+            Assert.AreEqual(true, tltrue.Value); // Always true, we rely on exceptions raised
+
+            using (var stream = new MemoryStream())
+            {
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Position = 0;
+
+                pos = 0;
+                tltrue = new TLTrue(stream, ref pos);
+                Assert.AreEqual(true, tltrue.Value); // Always true, we rely on exceptions raised
+            }
+
+        }
+
+    }
+}
---
+++ MTProto/TL/TLTrue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTProto.TL
+{
+    public class TLTrue : TLObject
+    {
+        private const int SIGNATURE = 0x3fedd339;
+
+        public bool Value { get; } = true;
+
+        public TLTrue() { }
+
+        public TLTrue(byte[] bytes, ref int position)
+        {
+            FromBytes(bytes, ref position);
+        }
+
+        public TLTrue(Stream input, ref int position)
+        {
+            FromStream(input, ref position);
+        }
+
+        public override TLObject FromBytes(byte[] bytes, ref int position)
+        {
+            bytes.ThrowIfIncorrectSignature(ref position, SIGNATURE);
+            return this;
+        }
+
+        public override TLObject FromStream(Stream input, ref int position)
+        {
+            var buffer = new byte[4];
+            input.Read(buffer, 0, 4);
+            buffer.ThrowIfIncorrectSignature(ref position, SIGNATURE);
+            return this;
+        }
+
+        public override byte[] ToBytes()
+        {
+            return BitConverter.GetBytes(SIGNATURE);
+        }
+
+        public override void ToStream(Stream input)
+        {
+            var bytes = ToBytes();
+            input.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
