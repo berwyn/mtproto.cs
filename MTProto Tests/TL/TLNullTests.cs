60ce484d61128b5b4f043af2a7dab52a0d3dd9d9
berwyn <berwyn.codeweaver@gmail.com>
2015-11-09 19:16:16 -0700

    Adds TLNull

---
+++ MTProto Tests/TL/TLNullTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MTProto.TL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTProto_Tests.TL
+{
+    [TestClass]
+    public class TLNullTests
+    {
+
+        [TestMethod]
+        public void TLNullSerializtion()
+        {
+            var expectedBuffer = BitConverter.GetBytes(0x56730bcc);
+            var actualBuffer = new TLNull().ToBytes();
+            CollectionAssert.AreEquivalent(expectedBuffer, actualBuffer);
+
+            using (var stream = new MemoryStream())
+            {
+                new TLNull().ToStream(stream);
+                stream.Position = 0;
+
+                actualBuffer = new byte[4];
+                stream.Read(actualBuffer, 0, 4);
+                CollectionAssert.AreEquivalent(expectedBuffer, actualBuffer);
+            }
+        }
+
+        [TestMethod]
+        public void TLNullHydration()
+        {
+            var pos = 0;
+            var buffer = BitConverter.GetBytes(0x56730bcc);
+            var tlnull = new TLNull(buffer, ref pos);
+            Assert.AreEqual(null, tlnull.Value);
+
+            using (var stream = new MemoryStream())
+            {
+                stream.Write(buffer, 0, 4);
+                stream.Position = 0;
+
+                pos = 0;
+                tlnull = new TLNull(stream, ref pos);
+                Assert.AreEqual(null, tlnull.Value);
+            }
+        }
+
+    }
+}
---
+++ MTProto/TL/TLNull.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTProto.TL
+{
+    public class TLNull : TLObject
+    {
+
+        private const int SIGNATURE = 0x56730bcc;
+
+        public object Value { get; } = null;
+
+        public TLNull() { }
+
+        public TLNull(byte[] bytes, ref int position)
+        {
+            FromBytes(bytes, ref position);
+        }
+
+        public TLNull(Stream input, ref int position)
+        {
+            FromStream(input, ref position);
+        }
+
+        public override TLObject FromBytes(byte[] bytes, ref int position)
+        {
+            bytes.ThrowIfIncorrectSignature(ref position, SIGNATURE);
+            return this;
+        }
+
+        public override byte[] ToBytes()
+        {
+            return BitConverter.GetBytes(SIGNATURE);
+        }
+
+        public override TLObject FromStream(Stream input, ref int position)
+        {
+            var buffer = new byte[4];
+            input.Read(buffer, 0, 4);
+            buffer.ThrowIfIncorrectSignature(ref position, SIGNATURE);
+            return this;
+        }
+
+        public override void ToStream(Stream input)
+        {
+            var buffer = ToBytes();
+            input.Write(buffer, 0, 4);
+        }
+    }
+}
